303a1798a6c440eadcf0d4ea8fc47bed63e74609
Oskar Lohse <oskar@baucampus.de>
2024-02-13 20:57:52 +0100

    test 9

--- resources/views/kontakt.blade.php
+++ resources/views/kontakt.blade.php
@@ -153,29 +153,24 @@ $p = 0;
                         <form method="POST" action="{{ route('contact.us.store') }}" id="contactUSForm">
                             {{ csrf_field() }}
                               
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
+                            <div class="form-field-two-column">
+                            <div class="form-field-wrapper">
                                         <strong>Name:</strong>
                                         <input type="text" name="name" class="form-control" placeholder="Name" value="{{ old('name') }}">
                                         @if ($errors->has('name'))
                                             <span class="text-danger">{{ $errors->first('name') }}</span>
                                         @endif
                                     </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
+                                    <div class="form-field-wrapper">
                                         <strong>Email:</strong>
                                         <input type="text" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}">
                                         @if ($errors->has('email'))
                                             <span class="text-danger">{{ $errors->first('email') }}</span>
                                         @endif
-                                    </div>
-                                </div>
+                                    </div>                                
                             </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
+                            <div class="form-field-two-column">
+                            <div class="form-field-wrapper">
                                         <strong>Phone:</strong>
                                         <input type="text" name="phone" class="form-control" placeholder="Phone" value="{{ old('phone') }}">
                                         @if ($errors->has('phone'))
@@ -183,19 +178,7 @@ $p = 0;
                                         @endif
                                     </div>
                                 </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <strong>Subject:</strong>
-                                        <input type="text" name="subject" class="form-control" placeholder="Subject" value="{{ old('subject') }}">
-                                        @if ($errors->has('subject'))
-                                            <span class="text-danger">{{ $errors->first('subject') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group">
+                            <div class="form-field-wrapper">
                                         <strong>Message:</strong>
                                         <textarea name="message" rows="3" class="form-control">{{ old('message') }}</textarea>
                                         @if ($errors->has('message'))
